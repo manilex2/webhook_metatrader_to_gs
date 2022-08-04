4d364bc996fa620331c3c2f59d27f40c0f96940d
avillao <villaoaron@gmail.com>
2022-08-04 05:41:49 -0500

    Se compló la funcionalidad en localhost.

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,28 +1,56 @@
+require('dotenv').config();
 const express = require('express');
-const PUERTO = 4300;
+var bodyParser = require('body-parser')
+
+var jsonParser = bodyParser.json()
+
+const PUERTO = 80;
 const app = express();
+
 const { google } = require('googleapis');
 const auth = new google.auth.GoogleAuth({
     keyFile: 'credentials.json',
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
 
-app.post('/', async (req, res) => {
+app.use(bodyParser.json());
+
+app.post('/',async (req, res) => {
   const client = await auth.getClient();
   const googleSheet = google.sheets({ version: 'v4', auth: client });
+  
+  let params = req.body;
+  
+  // Obtención de parametros
+  let id_bot = params.id;
+  let ticket = params.ticket;
+  let date = params.date;
+  let type = params.type;
+  let size = params.size;
+  let price = params.price;
+  let stopLoss = params.stop_loss;
+  let takeprofit = params.take_profit;
+  let profit = "";
+  let balance = "";
+  
+  if(params.hasOwnProperty('profit')){
+    profit = params.profit;
+    balance = params.balance;
+  }
+
   let datos = [];
-  datos.push(["fila1"]);
+  datos.push([ticket,date,type,size,price,stopLoss,takeprofit,profit,balance,id_bot]);
+
   await googleSheet.spreadsheets.values.append({
     /*auth: auth,*/
-    spreadsheetId: "",
-    range: "A:A",
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: process.env.ID_HOJA_RANGO,
     valueInputOption: "USER_ENTERED",
     requestBody: {
-        "range": "A:A",
+        "range": process.env.ID_HOJA_RANGO,
         "values": datos
     }
   });
-  //console.log('Datos agregados correctamente.');
   res.send("Datos agregados correctamente.");
 
 });
